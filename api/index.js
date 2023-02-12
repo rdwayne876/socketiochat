492609d98b45c4ab8e63ee109f96819fe93433dd
Dwayne R <rdwayne876@gmail.com>
2023-02-12 14:32:51 -0500

    Working chat app

--- api/index.js
+++ api/index.js
@@ -2,9 +2,23 @@ const express = require( 'express')
 const app = express()
 const http = require('http')
 const server = http.createServer(app)
+const path = require( 'path')
+const { Server} = require( "socket.io")
+const io = new Server(server)
 
 app.get('/', (req, res) => {
-    res.send('<h1>Hello World</h1>')
+    res.sendFile(path.join(__dirname, '..', 'app', 'index.html'))
+})
+
+io.on('connection', ( socket) => {
+    console.log( "a user is connected");
+    socket.on( 'disconnect', () => {
+        console.log( 'user disconnected');
+    })
+    socket.on( 'chat message', ( msg) => {
+        io.emit( 'chat message', msg)
+        console.log( 'message: ' + msg);
+    })
 })
 
 server.listen( 3000, () => {
